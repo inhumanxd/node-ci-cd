ba67b7208d5a0eb3d4612d377fae4bbab29b35b8
Bhavik Brahmbhatt <iambhavik1999@gmail.com>
2022-03-05 11:38:30 +0530

    Updated node server to not run in test mode.

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ app.get("/", (req, res) => {
 // Run Server
 const port = process.env.PORT || 3000;
 try {
-  app.listen(port, () => console.log(`Server is running on port: ${port}`));
+  if (process.env.NODE_ENV_MODE != "test") {
+    app.listen(port, () => console.log(`Server is running on port: ${port}`));
+  }
 } catch {
   console.log("Couldn't kickstart the server.");
 }
--- test/test.js
+++ test/test.js
@@ -2,11 +2,9 @@ const app = require("../index");
 const supertest = require("supertest");
 const request = supertest(app);
 
-describe("GET /", () => {
-  it("should respond with Yo! Wassup, Mate!", async (done) => {
+describe("GET /", () =>
+  it("should respond with Yo! Wassup, Mate!", async () => {
     const response = await request.get("/");
     expect(response.status).toBe(200);
     expect(response.text).toBe("Yo! Wassup, Mate!");
-    done();
-  });
-});
+  }));
