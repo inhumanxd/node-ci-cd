7706a5e83807d850adeaf98a0c62b95ac518a297
Bhavik Brahmbhatt <iambhavik1999@gmail.com>
2022-03-05 10:45:30 +0530

    Updated test.

--- test/test.js
+++ test/test.js
@@ -3,9 +3,10 @@ const supertest = require("supertest");
 const request = supertest(app);
 
 describe("GET /", () => {
-  it("should respond with Yo! Wassup, Mate!", async () => {
+  it("should respond with Yo! Wassup, Mate!", async (done) => {
     const response = await request.get("/");
     expect(response.status).toBe(200);
     expect(response.text).toBe("Yo! Wassup, Mate!");
+    done;
   });
 });
